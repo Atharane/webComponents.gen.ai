bc6f662b3c2ddd01382f8b1fb39edda56866bbae
Restyled.io <commits@restyled.io>
2024-03-26 07:42:52 +0000

    Restyled by prettier

--- app/models/User.js
+++ app/models/User.js
@@ -1,8 +1,7 @@
-
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name : {type : String, required : true},
+  name: { type: String, required: true }
 });
 
 const User = mongoose.model('user_details', userSchema);
--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const next = require('next');
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({dev});
+const app = next({ dev });
 const handle = app.getRequestHandler();
 const User = require('./app/models/User');
 
 const mongoose = require('mongoose');
-const {data} = require('autoprefixer');
-const {log} = require('console');
+const { data } = require('autoprefixer');
+const { log } = require('console');
 
 const MONGODB_URI =
-    'mongodb+srv://hpmahimkar:y0l0C4Hmqvq5w8nq@cluster0.uf3ieq3.mongodb.net/USERS';
+  'mongodb+srv://hpmahimkar:y0l0C4Hmqvq5w8nq@cluster0.uf3ieq3.mongodb.net/USERS';
 
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser : true,
-      useUnifiedTopology : true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
     });
     console.log('MongoDB connected successfully');
   } catch (error) {
@@ -31,20 +31,21 @@ app.prepare().then(() => {
   // Define custom routes or middleware if needed
   server.get('/getdummy', async (req, res) => {
     try {
-      const data = await User.find({name : "Hem"});
-      console.log(data, "data");
+      const data = await User.find({ name: 'Hem' });
+      console.log(data, 'data');
     } catch (error) {
-      console.log("error:", error);
+      console.log('error:', error);
     }
   });
 
   // Default route handler
-  server.all('*', (req, res) => { return "hello world"; });
+  server.all('*', (req, res) => {
+    return 'hello world';
+  });
 
   // Start the server
   server.listen(5000, (err) => {
-    if (err)
-      throw err;
+    if (err) throw err;
     console.log('> Ready on http://localhost:5000');
     connectDB();
   });
