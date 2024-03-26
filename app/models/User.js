0ff876caa6f42b00ddf50244d7e741bb3f6b3fa6
Restyled.io <commits@restyled.io>
2024-03-26 07:42:41 +0000

    Restyled by clang-format

--- app/models/User.js
+++ app/models/User.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+  name : {type : String, required : true},
 });
 
 const User = mongoose.model('user_details', userSchema);
